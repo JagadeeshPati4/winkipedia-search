9ee817f6b69db12f77b0ff0eaaca01beafe569e8
user266320 <jagadeeshpati4@gmail.com>
2024-04-16 15:49:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { Audio } from 'react-loader-spinner'
+import { useState } from 'react';
 import './App.css';
+import SearchResult from './search-result/indec';
 
 function App() {
+  const [searchValue, setsearch]=useState('')
+  const [search_results, setResults]=useState([])
+  const [loading, setloading]=useState(false)
+  const changeInput=(event)=>{
+    setsearch(event.target.value)
+    
+  }
+  const searchInput=(event)=>{
+    if (event.key === 'Enter'){
+      setloading(true)
+      let url = "https://apis.ccbp.in/wiki-search?search=" + searchValue;
+      
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (jsonData) {
+          let { search_results } = jsonData;
+          if (search_results !== null || search_results !== undefined){
+            setResults(search_results)
+          }
+          
+          setloading(false)
+        });
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="wiki-search-header">
+        <img className="wiki-logo" src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/wiki-logo-img.png" alt='winiki' />
+        <br />
+        <input onKeyDown={searchInput} onChange={changeInput} placeholder="Type a keyword and press Enter to search" type="search" className="search-input " id="searchInput" value={searchValue}/>
+      </div>
+      <div className='results-container'>
+        { loading  ?  <div className='loading'>
+       <Audio
+        height="50"
+        width="50"
+        radius="9"
+        color="black"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass/> 
+      </div>:<div className="search-results" id="searchResults">
+        {search_results.length >0 ?<div>{search_results.map(each =>(
+          <SearchResult result={each}/>
+        ))}</div> :<div></div>}
+      </div>}
+      
+      </div>
     </div>
   );
 }
---
+++ src/search-result/indec.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+const SearchResult = (props) => {
+    const {result} =props
+    const { link, title, description } = result;
+  return (
+    <div className='result-container'>
+      <a className='title' href={link} target='_blank' rel='noopener'> {title}</a>
+      <br/>
+      <a className='link' href={link} target='_blank' rel='noopener'>{link}</a>
+      <br/>
+      <p>{description}</p> 
+    </div>
+  )
+}
+
+export default SearchResult
